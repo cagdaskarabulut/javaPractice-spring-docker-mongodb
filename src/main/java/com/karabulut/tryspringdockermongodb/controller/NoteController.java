4b99b9b3624bc5046b2eb607a5bc0bd240560b63
cagdas_karabulut <ckarabulut@adersystems.com>
2023-03-12 22:53:00 +0300

    save method added

--- src/main/java/com/karabulut/tryspringdockermongodb/controller/NoteController.java
+++ src/main/java/com/karabulut/tryspringdockermongodb/controller/NoteController.java
@@ -4,6 +4,7 @@ import com.karabulut.tryspringdockermongodb.model.Note;
 import com.karabulut.tryspringdockermongodb.repository.NoteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,10 +19,13 @@ public class NoteController {
     @Autowired
     NoteRepository noteRepository;
 
-    @GetMapping("/all")
-    public List<Note> getAllNotes() {
-        return noteRepository.findAll();
+    @PostMapping("/")
+    public ResponseEntity<Note> addNote(@RequestBody Note note) {
+        return ResponseEntity.ok(noteRepository.save(note));
     }
 
-
+    @GetMapping("/all")
+    public ResponseEntity<List<Note>> getNotes() {
+        return ResponseEntity.ok(noteRepository.findAll());
+    }
 }
